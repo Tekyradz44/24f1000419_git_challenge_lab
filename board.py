28b6586b3032c9e480d1e70af329273ec77795c7
Tekyradz44 <datascience@andrewjohn.in>
2025-09-15 13:09:40 +0400

    Improved board visuals

--- board.py
+++ board.py
@@ -2,7 +2,7 @@
 def print_board(board):
     for row in board:
         print(" | ".join(row))
-        print("-" * 5)
+        print("-" * 10)
 
 def update_board(board, row, col, symbol):
     board[row][col] = symbol
